f3f2810d80002d767792bdcd3e14b45e0dc239fa
semastep96 <semastep5@gmail.com>
2023-01-17 17:35:19 +0300

    warning icon scaling on hover

---
+++ src/mui/appTheme.ts
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material';
+
+export const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#5533c3',
+    },
+    secondary: {
+      main: '#aba73b',
+    },
+    background: {
+      default: '#f6f2f0',
+    },
+    info: {
+      main: '#005096',
+    },
+    text: {
+      primary: '#1a1423',
+    },
+    error: {
+      main: '#b60000',
+    },
+    success: {
+      main: '#B2E27B',
+    },
+    warning: {
+      main: '#ffc971',
+    },
+  },
+});
