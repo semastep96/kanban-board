d44ba78af689a951b901329f1cec76299fd389ba
semastep96 <semastep5@gmail.com>
2023-01-18 23:52:19 +0300

    app bar + scroll to top hook

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,19 +1,14 @@
-import { Button } from '@mui/material';
 import React, { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { TopBar } from '../components/TopBar';
+import { useScrollToTop } from '../hooks/useScrollToTop';
 
 export const Root: FC = () => {
-  const navigate = useNavigate();
-  const {signout} = useAuth();
-  const handleSignout = () => {
-    signout(() => navigate(-1));
-  };
+  useScrollToTop();
   return (
-    <div className="App">
+    <div className="App" style={{ minHeight: '100vh' }}>
+      <TopBar />
       <h1>Root page</h1>
-      <Button variant='contained' onClick={handleSignout}>Logout</Button>
       <Outlet />
     </div>
   );
