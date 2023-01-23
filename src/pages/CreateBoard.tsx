d310d8b0f1fa665bee694224733b21650115fa78
semastep96 <semastep5@gmail.com>
2023-01-23 16:10:47 +0300

    fix lint

--- src/pages/CreateBoard.tsx
+++ src/pages/CreateBoard.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { CreateBoardForm } from '../components/forms/CreateBoardForm';
 
 export const CreateBoard = () => {
